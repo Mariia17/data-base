97ce966a6317f42e0e4a97676c3b35b5e72ca0ee
Mariia17 <marija170293@gmail.com>
2020-10-24 22:33:17 +0200

    Добавление метода генерации ID

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import * as fs from "fs";
 import path from "path";
+
+type JSONPrimitive = string | number | boolean | null;
+type JSONObject = { [x: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = JSONPrimitive | JSONObject | JSONArray;
+interface IUserObject {
+  userId: string;
+  userData: JSONObject;
+}
+
 class Database {
-  path: string;
+  private readonly path: string;
   constructor(path: string) {
     this.path = path;
     fs.writeFile(path, "[]", { flag: "wx+" }, (err) => {
@@ -16,28 +26,31 @@ class Database {
       }
     });
   }
-  addUser(userData: { name: string; surname: string; age: string; occupation: string }) {
+  addUser(userData: JSONObject) {
     fs.readFile(this.path, { encoding: "utf8", flag: "r+" }, (err, data) => {
       if (err) throw err;
-      const database = JSON.parse(data);
-      const userId = userData.name + userData.surname;
-      const userObject = { userId: userId, userData: userData };
-      const isNewUser = !database.some((item: { userId: string }) => item.userId === userId);
-      if (isNewUser) {
-        database.push(userObject);
-        fs.writeFile(this.path, JSON.stringify(database), { flag: "w+" }, (err) => {
-          if (err) throw err;
-          console.log("File saved!");
-        });
-      } else {
-        console.log("User already exists");
-      }
+
+      const database: IUserObject[] = JSON.parse(data);
+      const userIdArray = database.map((item) => item.userId);
+      const userId = this.generateId(userIdArray);
+      const userObject: IUserObject = { userId: userId, userData: userData };
+      database.push(userObject);
+
+      fs.writeFile(this.path, JSON.stringify(database), { flag: "w+" }, (err) => {
+        if (err) throw err;
+        console.log("File saved!");
+      });
     });
+  }
+  private generateId(id: string[]): string {
+    const userId = Math.random().toString();
+    if (!id.includes(userId)) return userId;
+    return this.generateId(id);
   }
 }
 
-const myDatabase = new Database(path.join(__dirname, "myBase.js"));
-myDatabase.addUser({ name: "Иван", surname: "Иванов", age: "30", occupation: "Механик" });
+const myDatabase = new Database(path.join(".", "myBase.json"));
+myDatabase.addUser({ name: "Иван", surname: "Иванов", age: "30", occupation: "s" });
 myDatabase.addUser({ name: "Иван", surname: "Иванов", age: "30", occupation: "Механик" });
 //myDatabase.changeUser("id", { ocuupation: "Инжинер" });
 //myDatabase.deleteUser("id");

aa107c93ab922aab96a1e91ee931cce7a60381f0
Mariia Zablodska <62555196+Mariia17@users.noreply.github.com>
2020-10-28 21:15:27 +0100

    Переписать addUser на промисы (#7)
    * Переписать addUser на промисы
    
    * Исправление addUser

--- index.ts
+++ index.ts
@@ -34,21 +34,26 @@ class Database {
   }
 
   addUser(userData: JSONObject) {
-    fs.readFile(this.path, { encoding: "utf8", flag: "r+" }, (err, data) => {
-      if (err) throw err;
-
-      const database: IUserObject[] = JSON.parse(data);
-      const userIdArray = database.map((item) => item.userId);
-      const userId = this.generateId(userIdArray);
-      const userObject: IUserObject = { userId: userId, userData: userData };
-      database.push(userObject);
-
-      fs.writeFile(this.path, JSON.stringify(database), { flag: "w+" }, (err) => {
-        if (err) throw err;
+    return promiseReadFile(this.path, { encoding: "utf8", flag: "r+" })
+      .then((data) => {
+        if (typeof data !== "string") {
+          throw new Error("string data expected");
+        }
+        const database: IUserObject[] = JSON.parse(data);
+        const userIdArray = database.map((item) => item.userId);
+        const userId = this.generateId(userIdArray);
+        const userObject: IUserObject = { userId: userId, userData: userData };
+        database.push(userObject);
+        return promiseWriteFile(this.path, JSON.stringify(database), { flag: "w+" });
+      })
+      .then(() => {
         console.log("File saved!");
+      })
+      .catch((err) => {
+        throw err;
       });
-    });
   }
+
   private generateId(id: string[]): string {
     const userId = Math.random().toString();
     if (!id.includes(userId)) return userId;

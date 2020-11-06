200d5b511df4673521fe593031ca855b93e0ad01
Mariia17 <marija170293@gmail.com>
2020-11-06 15:26:42 +0300

    ISS-8: Write tests for promiseReadFile

--- src/__mocks__/fs.ts
+++ src/__mocks__/fs.ts
@@ -1,13 +1,14 @@
 interface FS {
   writeFile: (path: unknown, data: unknown, options: unknown, callback: (err: Error | null) => void) => void;
+  readFile: (path: unknown, options: unknown, callback: (err: Error | null, data: string | null) => void) => void;
 }
 const fs = jest.createMockFromModule<FS>("fs");
 
-export const shouldWriteFileFail = { value: false };
+export const __testConfig = { shouldWriteFileFail: false, shouldReadFileFail: false };
 
 fs.writeFile = (path, data, options, callback) => {
   setTimeout(() => {
-    if (shouldWriteFileFail.value) {
+    if (!__testConfig.shouldWriteFileFail) {
       callback(null);
     } else {
       callback(new Error());
@@ -15,4 +16,15 @@ fs.writeFile = (path, data, options, callback) => {
   }, 0);
 };
 
+fs.readFile = (path, options, callback) => {
+  setTimeout(() => {
+    if (!__testConfig.shouldReadFileFail) {
+      callback(null, "data");
+    } else {
+      callback(new Error(), null);
+    }
+  }, 0);
+};
+
 export const writeFile = fs.writeFile;
+export const readFile = fs.readFile;
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,4 @@
 import Database from "./index";
-import { promiseWriteFile } from "./utils";
-
-import * as fs from "fs";
-
-jest.mock("fs");
 
 test("test", () => {
   const a = new Database("./myBase.js");
@@ -12,23 +7,3 @@ test("test", () => {
   expect(a.path).toEqual("./myBase.js");
 });
 
-test("writeFile", () => {
-  const _writeFile = jest.spyOn(fs, "writeFile");
-  return promiseWriteFile("somepath", "data", "options").finally(() =>
-    expect(_writeFile).toHaveBeenCalledWith("somepath", "data", "options", expect.any(Function))
-  );
-});
-
-test("isPromiseResolves", () => {
-  // @ts-ignore
-  fs.shouldWriteFileFail.value = false;
-  const result = promiseWriteFile("somepath", "data", "options");
-  return expect(result).resolves.toBe(undefined);
-});
-
-test("isPromiseRejects", () => {
-  // @ts-ignore
-  fs.shouldWriteFileFail.value = true;
-  const result = promiseWriteFile("somepath", "data", "options");
-  return expect(result).rejects.toThrow();
-});

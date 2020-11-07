ea15e50e9359e744b84e4b2f4b74704c176cd6b6
Mariia17 <marija170293@gmail.com>
2020-11-07 19:22:05 +0100

    Покрытие тестами метода init

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,32 +4,32 @@ import { mocked } from "ts-jest/utils";
 jest.mock("./utils");
 
 const _promiseWriteFile = mocked(promiseWriteFile);
-
-test("Check if path matches", () => {
-  const a = new Database("./myBase.js");
-  // @ts-ignore
-  expect(a.path).toEqual("./myBase.js");
-});
-
-test("Check if promiseWriteFile is called", () => {
-  const database = new Database("./myBase");
-
-  _promiseWriteFile.mockReturnValue(Promise.resolve());
-  return database.init().finally(() => {
-    expect(_promiseWriteFile).toHaveBeenCalled();
+describe("tests for init method", () => {
+  test("Check if path matches", () => {
+    const a = new Database("./myBase.js");
+    // @ts-ignore
+    expect(a.path).toEqual("./myBase.js");
   });
-});
 
-test("Check if nothing happens when file already exists  ", () => {
-  const database = new Database("./myBase");
+  test("Check if promiseWriteFile is called", () => {
+    const database = new Database("./myBase");
 
-  const err = { code: "EXIST" };
-  _promiseWriteFile.mockReturnValue(Promise.reject(err));
-  return expect(database.init()).resolves.toBe(undefined);
-});
+    _promiseWriteFile.mockReturnValue(Promise.resolve());
+    return database.init().finally(() => {
+      expect(_promiseWriteFile).toHaveBeenCalled();
+    });
+  });
 
-test("Promise should be rejected if error", () => {
-  const database = new Database("./myBase");
-  _promiseWriteFile.mockReturnValue(Promise.reject(new Error()));
-  return expect(database.init()).rejects.toThrow();
+  test("Check if nothing happens when file already exists  ", () => {
+    const database = new Database("./myBase");
+    const err = { code: "EXIST" };
+    _promiseWriteFile.mockReturnValue(Promise.reject(err));
+    return expect(database.init()).resolves.toBe(undefined);
+  });
+
+  test("Promise should be rejected if error", () => {
+    const database = new Database("./myBase");
+    _promiseWriteFile.mockReturnValue(Promise.reject(new Error()));
+    return expect(database.init()).rejects.toThrow();
+  });
 });
